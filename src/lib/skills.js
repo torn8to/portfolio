edde33bb9dbae76d0cc5180851fe2ef770a79a1f
Nathan Rogers <nathanroger314@gmail.com>
2026-01-01 13:17:19 -0500

    resume and skills update

--- src/lib/skills.js
+++ src/lib/skills.js
@@ -4,8 +4,8 @@ const skills = {
 	Robotics: ["ROS/ROS2", "RVIZ", "Gazebo", "Kicad"],
 	AI: ["Deep Learning", "Knowledge Distillation for Depth Estimation", "PPO"],
 	Databases: ["SQLite", "PostgreSQL"],
-	Tools: ["freeRTOS", "GDB", "Valgrind", "Nsight Compute"],
-	Other: ["v4l2", "Switch Kicks"]
+	Tools: ["GDB", "Valgrind", "Nsight Compute"],
+	Other: ["v4l2", "Switch Kicks", "PCB design", "Electronics"]
 };
 
 export default skills;
